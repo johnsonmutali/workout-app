0195f5bd789ba3c3a796ff80853fe0067f1c01f4
johnny-hyde <mutalijohnson34@gmail.com>
2024-05-15 22:20:44 +0300

    Set up react

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const cors = require("cors")
 require("dotenv").config()
 
 const workoutRoutes = require("./routes/workoutRoutes")
@@ -8,7 +9,13 @@ const app = express()
 const dbURI = process.env.DB_URI
 
 //middleware
+//allow requests from all origins
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+//for post requests
 app.use(express.json())
+//use the workoutRoutes 
 app.use(workoutRoutes)
 
 mongoose.connect(dbURI)
--- Frontend/src/components/Gym/index.jsx
+++ Frontend/src/components/Gym/index.jsx
@@ -4,19 +4,41 @@ import {
 } from "react"
 
 export default function Gym() {
+  //states
   const [workouts, setWorkouts] = useState(null)
+  //fetch the data.
+  //we use useEffect for this
   useEffect(() => {
-    const fetchWorkouts = async () => {
-      const response = await fetch("gym")
-      //const data = await response.json()
-      console.log(response)
-      if (response.ok) {
-        setWorkouts(response)
+    const fetchData = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/")
+        if (res.ok) {
+          const data = await res.json()
+          setWorkouts(data)
+        }
+      } catch (err) {
+        console.log("error: ", err)
       }
     }
 
-    fetchWorkouts()
+    fetchData()
   }, [])
 
-  return <h3> gym </h3 >
+  console.log("workouts: ", workouts)
+  return (
+    <div id="gym">
+      <ul class="workout-list">
+        <h1>workout list</h1>
+        {
+          workouts && workouts.map(workout => (
+            <li class="workout" key={workout._id}>
+              <h3 class="workout-title">{workout.title}</h3>
+              <p>preps: {workout.preps}</p>
+              <p>load: {workout.load}</p>
+            </li>
+          ))
+        }
+      </ul>
+    </div >
+  )
 }
